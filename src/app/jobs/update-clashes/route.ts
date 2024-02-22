c64ec296e435c52d623a49ee472ffa648044e282
Bradley Suira <bradleysuira@gmail.com>
2024-02-22 18:08:30 -0500

    fix: fixed sql query to set winners

--- src/app/jobs/update-clashes/route.ts
+++ src/app/jobs/update-clashes/route.ts
@@ -16,26 +16,27 @@ export async function GET(request: NextRequest) {
       update clashes
       set channel_winner_id = winners.winner_id
       from (
-      select s.clash_id,
-          case when s.correct_percentage_ch1 >s. correct_percentage_ch2 then s.channel1_id 
-            when s.correct_percentage_ch2 > s.correct_percentage_ch1 then s.channel2_id
-          else s.channel1_id --whe should decide if the have the same % 
-          end as winner_id
-      from (select c.id as clash_id,
-              c.channel1_id, 
-              c.channel2_id,
-              (case when ch1.question_id is null then null else cast(sum(case when ch1.correct_response='True' then 1 else 0 end) / count(ch1.question_id) as integer) * 100 end) as correct_percentage_ch1,
-              (case when ch2.question_id is null then null else cast(sum(case when ch2.correct_response='True' then 1 else 0 end) / count(ch2.question_id) as integer) * 100 end) as correct_percentage_ch2
-        from clashes c
-        left join user_question_responses ch1 
-        on ch1.channel_id = c.channel1_id
-        left join user_question_responses ch2 
-        on ch2.channel_id = c.channel2_id
-        where c.channel_winner_id IS null
-        and c.question_id = ${QUESTION_ID}
-        --WHERE q.expires_at::timestamp AT TIME ZONE 'MST' < current_timestamp AT TIME ZONE 'MST';
-        group by c.id, c.channel1_id, c.channel2_id, ch1.question_id, ch2.question_id
-        order by channel1_id, channel2_id) as s
+        select s.clash_id,
+        case when s.correct_percentage_ch1 > s.correct_percentage_ch2 then s.channel1_id 
+          when s.correct_percentage_ch2 > s.correct_percentage_ch1 then s.channel2_id
+        else (concat('[0:1]={', s.channel1_id, ',', s.channel2_id, '}')::int[])[trunc(random() * 2)::int] 
+        end as winner_id
+    from (select c.id as clash_id,
+            c.channel1_id, 
+            c.channel2_id,
+            (sum(case when ch1.correct_response is not true then 0 else 1 end)::decimal / count(coalesce(ch1.question_id, 0))::decimal) * 100 as correct_percentage_ch1,
+            (sum(case when ch2.correct_response is not true then 0 else 1 end)::decimal / count(coalesce(ch2.question_id, 0))::decimal) * 100 as correct_percentage_ch2
+      from clashes c
+      left join user_question_responses ch1 
+      on ch1.channel_id = c.channel1_id
+      and ch1.question_id = c.question_id
+       left join user_question_responses ch2 
+      on ch2.channel_id = c.channel2_id
+      and ch2.question_id = c.question_id
+      where c.question_id = 1
+      --WHERE q.expires_at::timestamp AT TIME ZONE 'MST' < current_timestamp AT TIME ZONE 'MST';
+      group by c.id, c.channel1_id, c.channel2_id
+      order by channel1_id, channel2_id) as s
       ) as winners 
       where winners.clash_id = id
       and channel_winner_id is null;`
