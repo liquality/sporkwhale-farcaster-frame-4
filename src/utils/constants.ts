6034f2d8277c9699d2e58d1164379513c7a2acc0
Bradley Suira <bradleysuira@gmail.com>
2024-02-23 15:11:51 -0500

    feat: using quetion id env var

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 // Configurations for the app
-export const QUESTION_ID = 1
-
+export const QUESTION_ID = parseInt(process.env.QUESTION_ID || '')
+console.log('QUESTION_ID:::::', QUESTION_ID)
 export const SUPPORTED_CHANNELS: Array<{
   id: number
   question_id: number
