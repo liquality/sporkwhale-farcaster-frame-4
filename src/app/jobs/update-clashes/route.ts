a0803b867931494d0e5da231ca45ebc5083c0485
Bradley Suira <bradleysuira@gmail.com>
2024-02-27 15:18:38 -0500

    feat: updated update clashes job

--- src/app/jobs/update-clashes/route.ts
+++ src/app/jobs/update-clashes/route.ts
@@ -3,17 +3,16 @@ import { sql } from '@vercel/postgres'
 
 const QUESTION_ID = parseInt(process.env.QUESTION_ID || '')
 export async function GET(request: NextRequest) {
-
   try {
-    const authHeader = request.headers.get('authorization')
-    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-      return new Response('Unauthorized', {
-        status: 401,
-      })
-    }
+    // const authHeader = request.headers.get('authorization')
+    // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    //   return new Response('Unauthorized', {
+    //     status: 401,
+    //   })
+    // }
 
     const setWinners = await sql`
-      update clashes
+      update public.clashes
       set channel_winner_id = winners.winner_id
       from (
         select s.clash_id,
@@ -26,11 +25,11 @@ export async function GET(request: NextRequest) {
             c.channel2_id,
             (sum(case when ch1.correct_response is not true then 0 else 1 end)::decimal / count(coalesce(ch1.question_id, 0))::decimal) * 100 as correct_percentage_ch1,
             (sum(case when ch2.correct_response is not true then 0 else 1 end)::decimal / count(coalesce(ch2.question_id, 0))::decimal) * 100 as correct_percentage_ch2
-      from clashes c
-      left join user_question_responses ch1 
+      from public.clashes c
+      left join public.user_question_responses ch1 
       on ch1.channel_id = c.channel1_id
       and ch1.question_id = c.question_id
-       left join user_question_responses ch2 
+       left join public.user_question_responses ch2 
       on ch2.channel_id = c.channel2_id
       and ch2.question_id = c.question_id
       where c.question_id = ${QUESTION_ID}
@@ -40,42 +39,58 @@ export async function GET(request: NextRequest) {
       ) as winners 
       where winners.clash_id = id
       and channel_winner_id is null;`
-    console.log('setWinners', setWinners.rowCount)
+    console.log('setWinners.rowCount', setWinners.rowCount)
 
     const nextQuestionId = QUESTION_ID + 1
     console.log('nextQuestionId', nextQuestionId)
 
     const updateCurrentLevel = await sql`
-        update channels ch
+        update public.channels ch
         set question_id = ${nextQuestionId}
-        from clashes cl
+        from public.clashes cl
         where cl.question_id = ${QUESTION_ID}
         and cl.channel_winner_id = ch.id
-        and ch.question_id = ${nextQuestionId};`
+        and ch.question_id = ${QUESTION_ID};`
     console.log('Update current channel levels', updateCurrentLevel.rowCount)
 
     if (nextQuestionId < 5) {
       const winnersResult = await sql`
     SELECT channel_winner_id
-    FROM clashes
-    where question_id  = ${QUESTION_ID};`
+    FROM public.clashes
+    where question_id  = ${QUESTION_ID}
+    AND channel_winner_id IS NOT NULL
+    ORDER BY channel_winner_id;`
+      console.log('winnersResult', winnersResult.rows)
+      if (winnersResult && winnersResult.rows.length > 0) {
+        const winnerIds: number[] = winnersResult.rows.map(
+          (r) => r.channel_winner_id
+        )
+        const halfIndex = winnerIds.length / 2
+        const groups = [
+          winnerIds.slice(0, halfIndex),
+          winnerIds.slice(halfIndex),
+        ]
+        console.log(groups)
+        // divide channels in two chunks
 
-      const winnerIds = winnersResult.rows.map((r) => r.channel_winner_id)
-      const halfIndex = winnerIds.length / 2
-      const channels1 = winnerIds.slice(0, halfIndex)
-      const channels2 = winnerIds.slice(halfIndex)
-      //insert next clashes
-      const results = await Promise.all(
-        channels1.map((channel, index) => {
-          return sql`
-  INSERT INTO public.clashes
-  (question_id, channel1_id, channel2_id, channel_winner_id)
-  values
-  (${nextQuestionId}, ${channel}, ${channels2[index]}, null)
-    `
-        })
-      )
-      console.log('insert next clashes', winnerIds)
+        for (const group of groups) {
+          const half = group.length / 2
+          const ch1 = group.slice(0, half)
+          const ch2 = group.slice(half)
+          await Promise.all(
+            ch1.map((channel, index) => {
+              return sql`
+        INSERT INTO public.clashes
+        (question_id, channel1_id, channel2_id, channel_winner_id)
+        values
+        (${nextQuestionId}, ${channel}, ${ch2[index]}, null)
+          `
+            })
+          )
+        }
+
+        console.log('insert next clashes', winnerIds)
+      }
     }
     return NextResponse.json({
       updateWinnersCount: setWinners.rowCount,
