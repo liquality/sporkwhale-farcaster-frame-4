ecfecda96470fd176251a01a95fc5c77828e6c8b
Bradley Suira <bradleysuira@gmail.com>
2024-02-29 11:19:49 -0500

    fix: fixed clashes generation

--- src/app/jobs/update-clashes/route.ts
+++ src/app/jobs/update-clashes/route.ts
@@ -4,38 +4,61 @@ import { sql } from '@vercel/postgres'
 const QUESTION_ID = parseInt(process.env.QUESTION_ID || '')
 export async function GET(request: NextRequest) {
   try {
-    const authHeader = request.headers.get('authorization')
-    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-      return new Response('Unauthorized', {
-        status: 401,
-      })
-    }
-    const winners = await sql`select s.clash_id,
-    case when s.correct_percentage_ch1 > s.correct_percentage_ch2 then s.channel1_id 
-      when s.correct_percentage_ch2 > s.correct_percentage_ch1 then s.channel2_id
-    else (concat('[0:1]={', s.channel1_id, ',', s.channel2_id, '}')::int[])[trunc(random() * 2)::int] 
-    end as winner_id
-from (select c.id as clash_id,
-        c.channel1_id, 
-        c.channel2_id,
-        (sum(case when ch1.correct_response is not true then 0 else 1 end)::decimal / count(coalesce(ch1.question_id, 0))::decimal) * 100 as correct_percentage_ch1,
-        (sum(case when ch2.correct_response is not true then 0 else 1 end)::decimal / count(coalesce(ch2.question_id, 0))::decimal) * 100 as correct_percentage_ch2
-  from clashes c
-  left join user_question_responses ch1 
-  on ch1.channel_id = c.channel1_id
-  and ch1.question_id = c.question_id
-   left join user_question_responses ch2 
-  on ch2.channel_id = c.channel2_id
-  and ch2.question_id = c.question_id
-  where c.question_id = ${QUESTION_ID}
-  --WHERE q.expires_at::timestamp AT TIME ZONE 'MST' < current_timestamp AT TIME ZONE 'MST';
-  group by c.id, c.channel1_id, c.channel2_id
-  order by channel1_id, channel2_id) as s;`
+    // const authHeader = request.headers.get('authorization')
+    // if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    //   return new Response('Unauthorized', {
+    //     status: 401,
+    //   })
+    // }
+    const responsesChannel1 = await sql`select c.id,
+    c.channel1_id as channel_id, 
+    count(qr.id) as correct_amount
+from clashes c
+join user_question_responses qr 
+on qr.channel_id = c.channel1_id
+and qr.question_id = c.question_id
+and qr.correct_response is true 
+where c.question_id = 1
+and c.channel_winner_id is null
+group by c.id, c.channel1_id;`
 
+    const responsesChannel2 = await sql`select c.id,
+    c.channel2_id as channel_id, 
+    count(qr.id) as correct_amount
+from clashes c
+join user_question_responses qr 
+on qr.channel_id = c.channel2_id
+and qr.question_id = c.question_id
+and qr.correct_response is true 
+where c.question_id = 1
+and c.channel_winner_id is null
+group by c.id, c.channel2_id;`
+
+    // merge the winners
+    const winners = responsesChannel1.rows.map((ch1: any) => {
+      const ch2 = responsesChannel2.rows.find((r) => r.id === ch1.id)
+      let winner_id
+      if (ch1.correct_amount > ch2?.correct_amount) {
+        winner_id = ch1.channel_id
+      } else if (ch2?.correct_amount > ch1.correct_amount) {
+        winner_id = ch2?.channel_id
+      } else {
+        const rand = Math.floor(Math.random() * (3 - 1) + 1)
+        winner_id = rand === 1 ? ch1.channel_id : ch2?.channel_id
+      }
+
+      return {
+        clash_id: ch1.id,
+        channel1_id: ch1.channel_id,
+        channel2_id: ch2?.channel_id,
+        winner_id,
+      }
+    })
+    console.log({ winners })
     const nextQuestionId = QUESTION_ID + 1
     console.log('nextQuestionId', nextQuestionId)
     const updateResults = await Promise.all(
-      winners.rows.map((winner: any) => {
+      winners.map((winner: any) => {
         return Promise.all([
           sql`
             update clashes
@@ -55,10 +78,10 @@ from (select c.id as clash_id,
     console.log('updateResults', updateResults)
 
     if (nextQuestionId < 5) {
-      console.log('winnersResult', winners.rowCount)
-      if (winners && winners.rows.length > 0) {
-        const winnerIds: number[] = winners.rows.map((r) => r.winner_id)
-        const len = winners.rows.length
+      console.log('winnersResult', winners.length)
+      if (winners && winners.length > 0) {
+        const winnerIds: number[] = winners.map((r) => r.winner_id)
+        const len = winners.length
         for (let index = 0; index < len; index += 2) {
           const ch1 = winnerIds[index]
           const ch2 = winnerIds[index + 1]
